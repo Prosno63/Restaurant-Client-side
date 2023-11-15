7778274a1b4f0fbf8fa73daa84621974d16126dd
Istiak38 <listiakahamad63@gmail.com>
2023-11-15 11:42:38 -0800

    Update Home.jsx

--- Resta-Client/src/Pages/Home.jsx
+++ Resta-Client/src/Pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Category from './Category/Category';
+import PopularMenu from './PopularMenu/PopularMenu';
+import Featured from './Featured/Featured';
+import Testy from './Testimonials/Testy';
 
 const Home = () => {
     return (
         <div>
 
             <Banner></Banner>
+            <Category></Category>
+            <PopularMenu></PopularMenu>
+            <Featured></Featured>
+            <Testy></Testy>
             
         </div>
     );
