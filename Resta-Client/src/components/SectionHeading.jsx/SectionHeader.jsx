fe0bdf60d4b73592dbccebba97b517cb97489e07
Istiak38 <listiakahamad63@gmail.com>
2023-11-15 11:41:54 -0800

    Create SectionHeader.jsx

---
+++ Resta-Client/src/components/SectionHeading.jsx/SectionHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SectionHeader = ({heading, subHeading}) => {
+    return (
+        <div className='mx-auto md:w-4/12 text-center mb-4'>
+
+            <p className='text-yellow-400 '>---{subHeading}---</p>
+            <h3 className='text-yellow-400 text-4xl uppercase border-y-4 py-2'>{heading}</h3>
+            
+        </div>
+    );
+};
+
+export default SectionHeader;
